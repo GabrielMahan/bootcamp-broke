37f6aaeca841148b820069f80c898387e3296414
Gabriel Mahan <mahan.gabe@gmail.com>
2016-10-24 14:06:40 -0400

    add initial basic components

---
+++ app/assets/javascripts/components/app.es6.jsx
@@ -0,0 +1,35 @@
+class App extends React.Component {
+
+  render(){
+    return(
+      <div className='app-container'>
+        <h1>This is the motherfucking page </h1>
+        <div className='ruminations'>
+          <h2> ruminations </h2>
+        </div>
+        <div className='learn-stuff'>
+          <h2> learn stuff</h2>
+        </div>
+        <div className='graffiti'>
+          <h2> Graffiti </h2>
+        </div>
+        <div className='seeking' >
+          <h2> Seeking Coder Budies </h2>
+        </div>
+
+
+        <div className="row">
+  <div className="col-xs-12 col-sm-6 col-md-8">.col-xs-12 .col-sm-6 .col-md-8</div>
+  <div className="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+</div>
+<div className="row">
+  <div className="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+  <div className="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+  <div className="clearfix visible-xs-block"></div>
+  <div className="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+</div>
+      </div>
+
+    )
+  }
+}
---
+++ app/assets/javascripts/components/header.es6.jsx
@@ -0,0 +1,10 @@
+class Header extends React.Component {
+  render(){
+    return(
+      <div className='header'>
+        <h1>BOOTCAMP BROKE</h1>
+        <h3>It's ok, none of us have jobs either</h3>
+      </div>
+    )
+  }
+}
